a3c425f22581f3eef6eef7c77ac85603df71d39d
Pieter Breed <github.com@pb.co.za>
2014-01-09 08:20:06 +0200

    default app version error showing version number

--- src/env_server/applications.clj
+++ src/env_server/applications.clj
@@ -6,18 +6,35 @@
 ;;; all in-memory
 (def apps-db (atom nil))
 
-(defn create-app-hash
+(defn -create-app-hash
     "creates a value-based hash of the data in the app"
     [data]
     (-> data hash str))
 
-(defn create-meta-info
+(defn -create-meta-info
     "creates some meta information that is not idempotent"
     [path data]
-    (let [ver (create-app-hash data)
+    (let [ver (-create-app-hash data)
           now (-> (time/date-time) time/as-map)]
           {:version-nr ver
            :created now}))
+
+(defn -get-app-or-error
+    "Returns the app indicated by path or an error"
+    [db path]
+    (let [apps (get db :apps {})]
+        (if (not (contains? apps path))
+            (throw+ {:type ::notfound :sub-type ::path :path path})
+            (get apps path))))
+
+(defn -get-app-version-or-error
+    "returns the specific app version record or an error"
+    [db path version]
+    (let [app (-get-app-or-error db path)
+          versions (get app :versions {})]
+        (if (not (contains? versions version))
+            (throw+ {:type ::notfound :sub-type ::version :path path :version version})
+            (get app version))))
 
 (defn post-app
     "Add the application identified by path to the db"
@@ -25,7 +42,7 @@
     (let [app (get-in db [:apps path] {})
           previous-versions (get app :versions {})
           previous-version (get app :current nil)
-          meta (assoc (create-meta-info path data)
+          meta (assoc (-create-meta-info path data)
                       :previous-version previous-version)
           new-version {:meta meta
                        :data data}
@@ -38,14 +55,10 @@
 (defn get-app
     "Gets the data from the app"
     ([db path version]
-        (let [apps (get db :apps {})]
-            (if (not (contains? apps path))
-                (throw+ {:type :notfound :sub-type :path :path path :version version})
-            (let [app-versions (get-in apps [path :versions] {})]
-                (if (not (contains? app-versions version))
-                    (throw+ {:type :notfound :sub-type :version :path path :version version})
-                (get app-versions version))))))
-    ([db path] (get-app path (get-in db [:apps path :current])))) ;; redirect to current version
+        (-get-app-version-or-error db path version))
+    ([db path] 
+        (let [app (-get-app-or-error db path)]
+            (throw+ {:type ::bad-request :sub-type ::version-not-specified :current-version (:current app)}))))
 
 
 
--- test/env_server/test/applications.clj
+++ test/env_server/test/applications.clj
@@ -1,14 +1,22 @@
 (ns env-server.test.applications
     (:use clojure.test
-          env-server.applications))
+          env-server.applications
+          [slingshot.slingshot :only [throw+ try+]]))
+
+(testing "testing"
+    (is true))
 
 (deftest addition
-    (testing "When adding apps to the db"
-        (testing "if the db is nil"
-            (let [path "path/to/file"
-                  data {:some :data}
-                  db (post-app nil path data)]
-                (testing "that the result is not nil also"
-                    (is (not (nil? db))))
-                (testing "that getting the default version will return the old data"
-                    (is data (get-app db path)))))))
+    (testing "When the first app to the db"
+        (let [path "path/to/file"
+              data {:some :data}
+              db (post-app nil path data)]
+            (testing "that the db is created"
+                (is (not (nil? db))))
+            (testing "that getting the default version returns an error with information to the current version"
+                (is "36047353" 
+                    (try+ (get-app db path)
+                        (catch [:type     :env-server.applications/bad-request 
+                                :sub-type :env-server.applications/version-not-specified] 
+                                {:keys [current-version]}
+                            current-version)))))))
